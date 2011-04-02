9aba01d2868be987ecbf3a805975c48f69482544
Gabriel Grise <gabriel@pieuvre.org>
2011-04-02 17:40:29 -0500

    accept command to the arduino

--- node/squirt.js
+++ node/squirt.js
@@ -1,8 +1,20 @@
 var express = require('express');
+var redis = require('redis');
 var app = express.createServer();
+var rclient = redis.createClient(6000, "localhost");
 
 app.get('/video', function(req, res){
   res.send("I'm some video");
+});
+app.post('/cmd/:type/:data', function(req, res) {
+	var command = {
+		"type":null,
+		"data":null
+	}
+	command.type = req.params.type;
+	command.data = req.params.data;
+	rclient.publish("canon_command",JSON.stringify(command));
+	res.send(JSON.stringify({"status":"OK"}));
 });
 
 app.configure(function(){
