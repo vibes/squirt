e031a6768b69ce77b67099398ec05e8152925dac
Anthony Fojas <developer@fojasaur.us>
2011-04-02 18:07:51 -0500

    front end to send to server based on scroll position

--- node/public/squirts.js
+++ node/public/squirts.js
@@ -1,3 +1,35 @@
-(function($){
+var Squirts = (function($){
+  var pageBody,
 
+  init = function(){
+    pageBody = $('body');
+
+    setTimeout(function() { 
+      // move screen to middle
+      window.scrollTo(360, 465);
+      // position the background video.
+      positionBG(); 
+    }, 0);
+
+    $(window).scroll(positionBG);
+    $(window).scroll(sendCoords);
+    
+  },
+
+  /*
+    mobile safari does not support position fixed.  we will manually do it
+  */
+  positionBG = function() {
+    $('#video-feed').css({top: (window.pageYOffset) + 'px', left:(window.pageXOffset) + 'px'});
+  },
+
+  sendCoords = function(){
+    $.post('/cmd/move/' + (pageBody.scrollLeft()) + ',' + (pageBody.scrollTop()));
+  };
+
+  return {
+    init:init
+  };
 })(jQuery);
+
+window.onload = Squirts.init;
